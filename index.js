1ae0d5ae02b37e97582875b71e45a249f43b581a
ScribblrBot <scribblrbot@gmail.com>
2025-09-10 13:46:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,9 @@
  */
 
 import { ApplicationCommandOptionType } from "@api/Commands/types";
-import definePlugin from "@utils/types";
+import definePlugin, { OptionType } from "@utils/types";
 
-// Load the local glitchy.json file
 import glitchyData from "./glitchy.json";
-// Or use: const glitchyData = require("./glitchy.json");
 
 export default definePlugin({
     name: "glitchkisser",
@@ -22,6 +20,19 @@ export default definePlugin({
             name: "Glitchy"
         }
     ],
+
+    options: Object.fromEntries(
+        Object.keys(glitchyData).map(type => [
+            `enable_${type}`,
+            {
+                type: OptionType.BOOLEAN,
+                description: `Enable type: ${type}`,
+                default: true,
+                restartNeeded: false
+            }
+        ])
+    ),
+
     commands: [
         {
             name: "glitchkisser",
@@ -34,13 +45,20 @@ export default definePlugin({
                     required: true
                 }
             ],
-            async execute(args) {
+            async execute(args, ctx) {
                 const type = args[0].value.toLowerCase();
                 const url = glitchyData[type];
 
                 if (!url) {
                     return {
-                        content: `\`${type}\ was not found rawr :(`
+                        content: `\`${type}\` was not found rawr :(`
+                    };
+                }
+
+                const enabled = ctx.options[`enable_${type}`];
+                if (!enabled) {
+                    return {
+                        content: `\`${type}\` is disabled in plugin options.`
                     };
                 }
 
